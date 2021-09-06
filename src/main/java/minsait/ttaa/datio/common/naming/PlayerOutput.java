07ae5fa3fdf170b133f163cccaafa545b1646ab6
yazdebi <62398257+yazdebi@users.noreply.github.com>
2021-09-06 08:11:51 -0500

    puntos, 1,2,3,4

--- src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
@@ -2,6 +2,11 @@ package minsait.ttaa.datio.common.naming;
 
 public final class PlayerOutput {
 
-    public static Field catHeightByPosition = new Field("cat_height_by_position");
+  //  public static Field catHeightByPosition = new Field("cat_height_by_position");
+    public static Field agerange  = new Field("age_range");
+    public static Field nationalityPosition = new Field("rank_by_nationality_position");
+  public static Field potentialvsoverall = new Field("potential_vs_overall");
+
+
 
 }
--- src/main/java/minsait/ttaa/datio/engine/Transformer.java
+++ src/main/java/minsait/ttaa/datio/engine/Transformer.java
@@ -1,5 +1,6 @@
 package minsait.ttaa.datio.engine;
 
+import minsait.ttaa.datio.common.naming.Field;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -23,7 +24,11 @@ public class Transformer extends Writer {
         df.printSchema();
 
         df = cleanData(df);
-        df = exampleWindowFunction(df);
+    //    df = exampleWindowFunction(df);
+        df = ageWindowFunction(df);
+        df = nationalityPositionWindowFunction(df);
+        df = potentialvsoverallWindowFunction(df);
+     //   exerciseWindowFunction
         df = columnSelection(df);
 
         // for show 100 records after your transformations and show the Dataset schema
@@ -37,10 +42,25 @@ public class Transformer extends Writer {
     private Dataset<Row> columnSelection(Dataset<Row> df) {
         return df.select(
                 shortName.column(),
-                overall.column(),
+                longName.column(),
+                age.column(),
                 heightCm.column(),
+                weightkg.column(),
+                nationality.column(),
+                clubname.column(),
+                overall.column(),
+                potential.column(),
                 teamPosition.column(),
-                catHeightByPosition.column()
+                agerange.column(),
+                nationalityPosition.column(),
+                potentialvsoverall.column()
+              //  catHeightByPosition.column()
+
+
+
+
+
+
         );
     }
 
@@ -91,10 +111,43 @@ public class Transformer extends Writer {
                 .when(rank.$less(50), "B")
                 .otherwise("C");
 
-        df = df.withColumn(catHeightByPosition.getName(), rule);
+     //   df = df.withColumn(catHeightByPosition.getName(), rule);
 
         return df;
     }
+
+    private Dataset<Row> ageWindowFunction(Dataset<Row> df) {
+
+        Column rule = when(age.column().$less(23), "A")
+                .when(age.column().$less(27), "B")
+                .when(age.column().$less(31), "C")
+           //     .when(rank.$greater(32), "D");
+                .otherwise("D");
+
+        df = df.withColumn(agerange.getName(), rule);
+
+        return df;
+    }
+
+    private Dataset<Row> nationalityPositionWindowFunction(Dataset<Row> df) {
+        WindowSpec w = Window
+                .partitionBy(nationality.column(),teamPosition.column())
+                .orderBy(overall.column().desc());
+
+           df = df.withColumn(nationalityPosition.getName(),  row_number().over(w));
+
+        return df;
+    }
+
+    private Dataset<Row> potentialvsoverallWindowFunction(Dataset<Row> df) {
+
+        Column d = potential.column().divide(overall.column());
+        df = df.withColumn(potentialvsoverall.getName(), d);
+
+        return df;
+    }
+
+
 
 
 
